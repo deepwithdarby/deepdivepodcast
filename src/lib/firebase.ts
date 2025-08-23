db6c6d7cd06c99cd4d7b467e38e3a8603825c0dd
deepwithdarby <deepwithdarby@gmail.com>
2025-08-23 08:50:53 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqkIZs8U3ZuJNvDh_w8a1m2gOwLaPNpyk",
-  authDomain: "podcast-app-7d98d.firebaseapp.com",
-  projectId: "podcast-app-7d98d",
-  storageBucket: "podcast-app-7d98d.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef1234567890"
+  apiKey: "AIzaSyAGWholubNmSE4wVI-ug08fAecmZQPpbRg",
+  authDomain: "music-streaming-website33.firebaseapp.com",
+  databaseURL: "https://music-streaming-website33-default-rtdb.firebaseio.com",
+  projectId: "music-streaming-website33",
+  storageBucket: "music-streaming-website33.firebasestorage.app",
+  messagingSenderId: "32814188597",
+  appId: "1:32814188597:web:a60c9de2d93183b77f097c",
+  measurementId: "G-NVYC6PNDWK"
 };
 
 const app = initializeApp(firebaseConfig);
